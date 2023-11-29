8d94aa67c08100777d6d9471d4909442adcda5c4
lmorv <kishongus@gmail.com>
2023-11-28 19:12:49 -0500

    Code: Iteration on motor control
    - I integrated some motor control into the atomata main script that held the capacitive sensing code for the bracelets and the automata.
    - This iterative manifestation allows us to set state configurations based on given thresholds of capacitive intensity coming from the bracelets. Those states affect the motor's speed in particular ways so that the users may experiment with an unpredictable set of results.

--- code/automata/automata.ino
+++ code/automata/automata.ino
@@ -2,13 +2,23 @@
 
 long automataCS;
 long bracelet1, bracelet2;
+int motorPin = 9;
+int braceletStates[2] = {0}; // index position (0 or 1) refers to either bracelet1 or bracelet2
 
 CapacitiveSensor cs_3_2 = CapacitiveSensor(3, 2);  // 10 megohm resistor between pins 4 & 2, pin 2 is sensor pin, add wire, foil
 CapacitiveSensor cs_7_6 = CapacitiveSensor(7, 6);
 CapacitiveSensor cs_11_10 = CapacitiveSensor(11, 10);
 
+enum {
+  A,
+  B,
+  C,
+  D
+} state;
+
 
 void setup() {
+  pinMode(motorPin, OUTPUT);
   Serial.begin(9600);
   Serial.println("start");
   cs_3_2.set_CS_AutocaL_Millis(0xFFFFFFFF);  // turn off autocalibrate on channel 1 - just as an example Serial.begin(9600);
@@ -17,11 +27,65 @@ void setup() {
 
 }
 
+void getBracelet(int b, int idb) {
+
+switch (b) {
+    case 30 ... 100:
+      if(!idb) braceletStates[0] = A;
+      if(idb) braceletStates[1] = A;
+      break;
+
+    case 101 ... 400:
+      if(!idb) braceletStates[0] = B;
+      if(idb) braceletStates[1] = B;
+      break;
+
+    case 401 ... 2000:
+      if(!idb) braceletStates[0] = A;
+      if(idb) braceletStates[1] = B;
+      break;
+
+    default:
+      if(!idb) braceletStates[0] = B;
+      if(idb) braceletStates[1] = A;
+      break;
+  }
+
+}
+
+void runBState() {
+
+  if (braceletStates[0] == A && braceletStates[1] == A) {
+      analogWrite(motorPin, 80);  // Low power to motor
+        Serial.println("Motor is running at a slower speed.");
+
+    } else if (braceletStates[0] == B && braceletStates[1] == B) {
+      analogWrite(motorPin, 175);  // Medium power to motor
+        Serial.println("Motor is running at medium speed.");
+
+    } else if (braceletStates[0] == A && braceletStates[1] == B) {
+      analogWrite(motorPin, 255);  // High power to motor
+        Serial.println("Motor is running at a higher speed.");
+
+    } else if (braceletStates[0] == B && braceletStates[1] == A) {
+      analogWrite(motorPin, 0);  // no power to motor
+        Serial.println("turn motor  off.");
+    };
+
+}
+
 void loop() {
 
   automataCS = cs_3_2.capacitiveSensor(30);
   bracelet1 = cs_7_6.capacitiveSensor(30);
   bracelet2 = cs_11_10.capacitiveSensor(30);
+
+  getBracelet(bracelet1, 0); // set the state of bacelet1 acording to value thresholds
+  getBracelet(bracelet2, 1);// set the state of bacelet2 acording to value thresholds
+
+  // Serial.println(braceletStates[0]);
+  
+  runBState(); // power motor at a set intensity for each state configuration
 
   Serial.print("autoCS: ");  // tab character for debug window spacing
   Serial.print(automataCS);  // print sensor output 1
